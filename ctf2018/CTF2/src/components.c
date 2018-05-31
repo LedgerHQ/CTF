a8560acea39ce8314337a24f73af58f96e3fb08e
Charles <charles@ledger.fr>
2018-05-31 14:20:53 +0200

    Add License

--- ctf2018/CTF2/src/components.c
+++ ctf2018/CTF2/src/components.c
@@ -1,3 +1,20 @@
+/*******************************************************************************
+*   Ledger CTF 2018 - Security Team
+*   (c) 2018 - Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
 // mixcolumns using precalculated multiplication tables
 inline void fastMix4bytes(u8 *a, u8 *b, u8 *c, u8 *d) {
   u8 aa, bb, cc, dd;
--- ctf2018/CTF2/src/genSboxes.c
+++ ctf2018/CTF2/src/genSboxes.c
@@ -1,3 +1,20 @@
+/*******************************************************************************
+*   Ledger CTF 2018 - Security Team
+*   (c) 2018 - Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+
 unsigned char zecrc[16] = {0x9b,0x04,0x57,0x44,0xe5,0x1f,0xf0,0xf9,0xe8,0x4a,0x54,0xda,0x68,0xe0,0x52,0xb8};
 
 void genSboxes(word8 keys[11][4][4], word8 masks[4][4]){
